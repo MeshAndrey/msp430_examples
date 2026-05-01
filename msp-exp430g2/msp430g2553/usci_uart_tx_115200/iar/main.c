941f704666ea72ad1a7a97949333b601bf521e04
MeshAndrey <35770825+MeshAndrey@users.noreply.github.com>
2026-05-01 12:01:47 +0300

    msp430g2553: Add usci uart 115200 tx example

---
+++ msp-exp430g2/msp430g2553/usci_uart_tx_115200/iar/main.c
@@ -0,0 +1,39 @@
+#include <msp430.h>
+#include <stdint.h>
+
+// MCLK is 1 048 576 ~ by default
+#define UART_DIV_L (uint8_t)(1048576 / 115200)
+
+int main(void)
+{
+  // Stop WDT
+  WDTCTL = WDTPW | WDTHOLD;
+  /* Port 1 Selection */
+  P1SEL |= BIT1 | BIT2 ;
+  /* Port 1 Selection 2 */
+  P1SEL2 |= BIT1 | BIT2;
+  // USCI A0 Control Register 1, USCI 0 Clock Source: 2  // 10b = SMCLK
+  UCA0CTL1 |= UCSSEL_2;
+  // 1MHz 115200
+  // USCI_Ax Baud-Rate Control 0 Register
+  UCA0BR0 = UART_DIV_L;
+  // USCI_Ax Baud-Rate Control 1 Register
+  UCA0BR1 = 0;
+  // Modulation UCBRSx = 1
+  // USCI A0 Modulation Control
+  // USCI Second Stage Modulation Select 0
+  UCA0MCTL = UCBRS0; // + UCBRS2
+  // **Initialize USCI state machine**
+  // USCI A0 Control Register 1, USCI Software Reset
+  UCA0CTL1 &= ~UCSWRST;
+  
+  uint8_t counter = 0;
+  
+  while (1)
+  {
+    UCA0TXBUF = counter; // send counter byte. printable char begins from '0'/0x30
+    counter++;
+    
+    __delay_cycles(1000000); // ~1 second delay
+  }
+}
